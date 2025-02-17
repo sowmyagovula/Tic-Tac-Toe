43e93c8c252c086185b7db9617d989a6b76d16ef
Sowmya Govula <sowmyagovula@gmail.com>
2025-02-17 16:03:46 +0530

    initial

---
+++ TicTacToe.py
@@ -0,0 +1,16 @@
+# Tic tac Toe Game
+
+# Importing the required libraries
+import random
+
+#Function to print the board
+
+def print_board(board):
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('---|---|---')
+    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('---|---|---')
+    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+
+def check(board, player):
+    pass
